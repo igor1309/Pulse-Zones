ac0a3d064d94c9ec880ad516236199dcfec59ccd
Igor Malyarov <igor1309@gmail.com>
2020-06-28 21:35:25 +0300

    tweaks

--- Pulse Zones/ContentView.swift
+++ Pulse Zones/ContentView.swift
@@ -7,6 +7,85 @@
 
 import SwiftUI
 
+struct GenderAgeView: View {
+    
+    @Binding var gender: String
+    @Binding var age: Double
+    
+    let genders = ["женский", "мужской", "бинарный", "другой"]
+    
+    var body: some View {
+        Group {
+            HStack {
+                Text("Пол")
+                
+                Picker("Пол", selection: $gender) {
+                    ForEach(genders, id: \.self) { gender in
+                        Text(gender).tag(0)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            HStack {
+                Text("Возраст \(Int(age))")
+                
+                Slider(value: $age, in: 15.0...100.0)
+            }
+        }
+        .foregroundColor(Color(UIColor.systemOrange))
+    }
+}
+
+struct ZonesView: View {
+    
+    let gender: String
+    let age: Double
+    let maxPulse: Double
+    
+    var body: some View {
+        Group {
+            HStack {
+                Image(systemName: "heart")
+                Text("Пульсовые зоны".uppercased())
+            }
+            .foregroundColor(.secondary)
+            .font(.subheadline)
+            
+            ForEach(zones) { zone in
+                DisclosureGroup("\(zone.name): \(Int(maxPulse * zone.min))" + " – " + "\(Int(maxPulse * zone.max))"
+                ) {
+                    Text(zone.description)
+                        .foregroundColor(.secondary)
+                        .font(.subheadline)
+                }
+                .foregroundColor(zone.color)
+                .font(.headline)
+            }
+        }
+    }
+}
+
+struct NotesView: View {
+    var body: some View {
+        Group {
+            HStack {
+                Image(systemName: "text.justify")
+                Text("Примечания".uppercased())
+            }
+            
+            ForEach(zoneTerms.indices, id: \.self) { index in
+                DisclosureGroup(zoneTerms[index].key) {
+                    Text(zoneTerms[index].value)
+                        .font(.footnote)
+                }
+            }
+        }
+        .foregroundColor(.secondary)
+        .font(.subheadline)
+    }
+}
+
 struct ContentView: View {
     
     @AppStorage("gender")
@@ -15,75 +94,29 @@ struct ContentView: View {
     @AppStorage("age")
     private var age: Double = 30
     
+    ///  Обобщенная формула подсчета МЧСС: 220 минус ваш возраст. Более современная формула: 214-(0.8 x возраст) для мужчин и 209-(0.9 x возраст) для женщин. Но более информативным будет получить значение в лабораторных условиях.
     var maxPulse: Double {
         gender == "мужской"
             ? 214 - (0.80 * age)
             : 209 - (0.90 * age)
     }
     
-    var genders = ["женский", "мужской", "бинарный", "другой"]
-    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-                HStack {
-                    Text("Пол")
-                    
-                    Picker("Пол", selection: $gender) {
-                        ForEach(genders, id: \.self) { gender in
-                            Text(gender).tag(0)
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                
-                HStack {
-                    Text("Возраст \(Int(age))")
-                    
-                    Slider(value: $age, in: 15.0...100.0)
-                }
+                GenderAgeView(gender: $gender, age: $age)
                 
                 Divider()
                 
                 ScrollView(showsIndicators: false) {
                     VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "heart")
-                            Text("Пульсовые зоны".uppercased())
-                        }
-                        .foregroundColor(.secondary)
-                        .font(.subheadline)
                         
-                        ForEach(zones) { zone in
-                            DisclosureGroup("\(zone.name): \(Int(maxPulse * zone.min))" + " – " + "\(Int(maxPulse * zone.max))") {
-                                Text(zone.description)
-                                    .foregroundColor(.secondary)
-                                    .font(.subheadline)
-                            }
-                            .foregroundColor(zone.color)
-                            .font(.headline)
-                        }
+                        ZonesView(gender: gender, age: age, maxPulse: maxPulse)
                         
                         Divider()
                         
-                        Group {
-                            HStack {
-                                Image(systemName: "text.justify")
-                                Text("Примечания".uppercased())
-                            }
-                            
-                            ForEach(zoneTerms.indices, id: \.self) { index in
-                                DisclosureGroup(zoneTerms[index].key) {
-                                    Text(zoneTerms[index].value)
-                                        .font(.footnote)
-                                }
-                            }
-                        }
-                        .foregroundColor(.secondary)
-                        .font(.subheadline)
+                        NotesView()
                     }
-                    
-                    Spacer()
                 }
             }
             .padding()
@@ -96,5 +129,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
+            .environment(\.sizeCategory, .large)
     }
 }
